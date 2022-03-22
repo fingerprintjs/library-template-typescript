bf3229e908b6a06f86f7e62571c61e729c53185e
Ilya Taratukhin <darkilfa@gmail.com>
2022-03-22 12:42:57 +0300

    Fix UMD and IIFE builds

--- rollup.config.js
+++ rollup.config.js
@@ -25,25 +25,50 @@ const commonInput = {
 }
 
 const commonOutput = {
+  // name: 'MyFpJsLibrary', // Need for IIFE build. Name of global variable
   exports: 'named',
 }
 
-// Need for IIFE build
+// Need for IIFE or UMD build
 // const commonTerser = terserPlugin(require('./terser.config.js'))
 
 export default [
+  // UMD and IIFE build config
+  // {
+  //   ...commonInput,
+  //   output: [
+  //     // IIFE build for browser with adding globals to window
+  //     {
+  //       ...commonOutput,
+  //       file: `${outputDirectory}/${artifactName}.js`,
+  //       format: 'iife',
+  //     },
+  //     {
+  //       ...commonOutput,
+  //       file: `${outputDirectory}/${artifactName}.min.js`,
+  //       format: 'iife',
+  //       plugins: [commonTerser],
+  //     },
+  //
+  //     // UMD for users who use Require.js or Electron and want to leverage them
+  //     {
+  //       ...commonOutput,
+  //       file: `${outputDirectory}/${artifactName}.umd.js`,
+  //       format: 'umd',
+  //     },
+  //     {
+  //       ...commonOutput,
+  //       file: `${outputDirectory}/${artifactName}.umd.min.js`,
+  //       format: 'umd',
+  //       plugins: [commonTerser],
+  //     },
+  //   ]
+  // },
   // NPM bundles. They have all the dependencies excluded for end code size optimization.
   {
     ...commonInput,
     external: Object.keys(dependencies),
     output: [
-      // IIFE build for browser with adding globals to window
-      // {
-      //   ...commonOutput,
-      //   file: `${outputDirectory}/fp.min.js`,
-      //   format: 'iife',
-      //   plugins: [commonTerser],
-      // },
       // CJS for usage with `require()`
       {
         ...commonOutput,
